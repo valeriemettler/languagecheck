79f81e085d8411d4aef6923550fd067f7729518d
Valerie Mettler <valeriemettler@gmail.com>
2014-10-02 17:12:20 -0500

    updated files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Languagecheck::Application.routes.draw do
+  get "welcome/index"
+  get "welcome/about"
+  root to: 'welcome#index' 
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +57,4 @@ Languagecheck::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
