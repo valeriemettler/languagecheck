fcd7535474deddac03878e7e52f208c39fea328e
Valerie Mettler <valeriemettler@gmail.com>
2014-10-05 17:16:58 -0500

    updated question controller and views

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,55 +1,70 @@
 class QuestionsController < ApplicationController
   before_action :set_question, only: [:show, :edit, :update, :destroy]
 
-  # GET /questions
-  # GET /questions.json
   def index
     @questions = Question.all
   end
 
-  # GET /questions/1
-  # GET /questions/1.json
   def show
+    @question = Question.find(params[:id])
   end
 
-  # GET /questions/new
   def new
     @question = Question.new
   end
 
-  # GET /questions/1/edit
   def edit
+    @question = Question.find(params[:id])
   end
 
-  # POST /questions
-  # POST /questions.json
+  def update
+     @question = Question.find(params[:id])
+     if @question.update_attributes(params.require(:question).permit(:body))
+       flash[:notice] = "Question was updated."
+       redirect_to @question
+     else
+       flash[:error] = "There was an error saving the question. Please try again."
+       render :edit
+     end
+   end
+
   def create
-    @question = Question.new(question_params)
+     @question = Question.new(params.require(:question).permit(:body))
+     if @question.save
+       flash[:notice] = "Question was saved."
+       redirect_to @question
+     else
+       flash[:error] = "There was an error saving the question. Please try again."
+       render :new
+     end
+   end
+  # def create
+  #   @question = Question.new(question_params)
 
-    respond_to do |format|
-      if @question.save
-        format.html { redirect_to @question, notice: 'Question was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @question }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @question.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #   respond_to do |format|
+  #     if @question.save
+  #       format.html { redirect_to @question, notice: 'Question was successfully created.' }
+  #       format.json { render action: 'show', status: :created, location: @question }
+  #     else
+  #       format.html { render action: 'new' }
+  #       format.json { render json: @question.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # PATCH/PUT /questions/1
   # PATCH/PUT /questions/1.json
-  def update
-    respond_to do |format|
-      if @question.update(question_params)
-        format.html { redirect_to @question, notice: 'Question was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @question.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+  #   respond_to do |format|
+  #     if @question.update(question_params)
+  #       format.html { redirect_to @question, notice: 'Question was successfully updated.' }
+  #       format.json { head :no_content }
+  #     else
+  #       format.html { render action: 'edit' }
+  #       format.json { render json: @question.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # DELETE /questions/1
   # DELETE /questions/1.json
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,72 +1,86 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, except: [:show]
+ 
+   def update
+     if current_user.update_attributes(user_params)
+       flash[:notice] = "User information updated"
+       redirect_to edit_user_registration_path
+     else
+       render "devise/registrations/edit"
+     end
+   end
+
   def index
     @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
-
-  def show
-  end
+private
+    def user_params
+      params.require(:user).permit(:name)
+    end
+end
 
   # GET /questions/new
-  def new
-    @user = User.new
-  end
+  # def new
+  #   @user = User.new
+  # end
 
   # GET /questions/1/edit
-  def edit
-  end
+  # def edit
+  # end
 
   # POST /questions
   # POST /questions.json
-  def create
-    @user = User.new(user_params)
+  # def create
+  #   @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @user }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #   respond_to do |format|
+  #     if @user.save
+  #       format.html { redirect_to @user, notice: 'User was successfully created.' }
+  #       format.json { render action: 'show', status: :created, location: @user }
+  #     else
+  #       format.html { render action: 'new' }
+  #       format.json { render json: @user.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # PATCH/PUT /questions/1
   # PATCH/PUT /questions/1.json
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+  #   respond_to do |format|
+  #     if @user.update(user_params)
+  #       format.html { redirect_to @user, notice: 'User was successfully updated.' }
+  #       format.json { head :no_content }
+  #     else
+  #       format.html { render action: 'edit' }
+  #       format.json { render json: @user.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # DELETE /questions/1
   # DELETE /questions/1.json
-  def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url }
-      format.json { head :no_content }
-    end
-  end
+  # def destroy
+  #   @user.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to users_url }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
-  private
+  # private
     # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
+    # def set_user
+    #   @user = User.find(params[:id])
+    # end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:id)
-    end
-end
+#     def user_params
+#       params.require(:user).permit(:id)
+#     end
+# end
